bda002057d92ff58c55f009d3d54655d2f99a607
Jorge Rodriguez <jarf4321@gmail.com>
2023-01-26 03:46:58 -0600

    Add-devise user feature
    - routes
    - model
    - table
    - Only logins users can visit other pages than the home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :authenticate_user! # Devise
 
   def set_locale
     I18n.locale = 'es'
--- db/schema.rb
+++ db/schema.rb
@@ -31,5 +31,16 @@ ActiveRecord::Schema.define(version: 20_230_112_214_129) do
     t.index ['category_id'], name: 'index_tasks_on_category_id'
   end
 
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  end
   add_foreign_key 'tasks', 'categories'
 end
