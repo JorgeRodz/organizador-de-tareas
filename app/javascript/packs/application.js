dd880fd1cfccf814a0bf25d68e1fcad4cb9d1f15
Jorge Rodriguez <jarf4321@gmail.com>
2023-02-10 09:38:47 -0600

    Add bootstrap and roboto-font dependencies
    - Add bootstrap necesary dependencies, along side bootstrap scss
    - Add roboto-fontface dependencies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,9 @@ require("channels");
 
 // *To import cocoon js code
 import "cocoon";
+import "bootstrap";
+import "roboto-fontface";
+import "../src/stylesheets/application";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
